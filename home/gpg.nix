a743902a4a736b8f37ddc69c5d50aa4b57964bf7
Tanjim H. <contact@audacioustux.com>
2026-01-02 01:52:44 +0600

    Add SSH key configuration for GPG agent

--- home/gpg.nix
+++ home/gpg.nix
@@ -5,6 +5,7 @@
   services.gpg-agent = {
     enable = true;
     enableSshSupport = true;
+    sshKeys = [ "DD4E6C0077DC0801CC34214FCE7998BA338A88CA" ];
     pinentry.package = pkgs.pinentry_mac;
   };
 }
